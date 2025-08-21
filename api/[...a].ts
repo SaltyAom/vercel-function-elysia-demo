dbc92aef0051a750d999bf137851734abf68a74f
saltyaom <saltyaom@gmail.com>
2025-08-21 23:40:01 +0700

    :tada: feat: init

---
+++ api/[...a].ts
@@ -0,0 +1,12 @@
+import { Elysia, t } from 'elysia'
+import { swagger } from '@elysiajs/swagger'
+
+export default new Elysia({ prefix: '/api' })
+    .use(swagger())
+    .get('/', 'hello')
+    .post('/mirror', ({ body }) => body, {
+        body: t.Object({
+            name: t.String()
+        })
+    })
+    .compile()
