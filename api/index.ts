bd0068c8fb6aa56d9ea323b3b3cd2c565a073236
saltyaom <saltyaom@gmail.com>
2025-08-21 23:35:20 +0700

    :tada: feat: init

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,8 @@
 import { Elysia, t } from 'elysia'
+import { swagger } from '@elysiajs/swagger'
 
 const app = new Elysia()
+    .use(swagger(())
     .get('/', 'hello')
     .post('/mirror', ({ body }) => body, {
 	body: t.Object({
